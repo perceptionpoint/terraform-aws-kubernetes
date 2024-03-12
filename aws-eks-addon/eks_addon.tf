0f2684cb90314e13ac24b5ea120a0100875d966a
Oron Gola <oron.gola@perception-point.io>
2024-03-12 20:30:19 +0200

    fix typos with aws-eks-addon module

--- aws-eks-addon/eks_addon.tf
+++ aws-eks-addon/eks_addon.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_addon" "eks_addon" {
     for_each = var.addon_properties
-  cluster_name = eks_properties.name
+  cluster_name = var.eks_properties["name"]
   addon_name = each.key
   addon_version = each.value
   resolve_conflicts_on_update = "PRESERVE"
--- aws-eks-addon/output.tf
+++
@@ -1,4 +0,0 @@
-output "cluster_addons" {
-  description = "Map of attribute maps for all EKS cluster addons enabled"
-  value       = merge(aws_eks_addon.this, aws_eks_addon.before_compute)
-}
