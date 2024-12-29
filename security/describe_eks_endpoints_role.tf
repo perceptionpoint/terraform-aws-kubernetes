b359eaea0bd1d0766a11d36444e7c1ac3c00eda6
Oron Gola <oron.gola@perception-point.io>
2024-12-29 21:13:21 +0200

    describeEksEndpoints can now be assumed with web identity using github actions idp

--- security/describe_eks_endpoints_role.tf
+++ security/describe_eks_endpoints_role.tf
@@ -1,3 +1,9 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  github_oidc_provider_name = "token.actions.githubusercontent.com"
+}
+
 resource "aws_iam_role" "describe_eks_endpoints" {
   count = var.describe_eks_endpoints_assuming_account_id == null? 0 : 1
 
@@ -19,6 +25,23 @@ data "aws_iam_policy_document" "describe_eks_endpoints_assume_role" {
       identifiers = ["arn:aws:iam::${var.describe_eks_endpoints_assuming_account_id}:root"]
     }
     actions = ["sts:AssumeRole"]
+  }
+  statement {
+    principals {
+      type = "Federated"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.id}:oidc-provider/${local.github_oidc_provider_name}"]
+    }
+    condition {
+      test = "StringLike"
+      variable = "${local.github_oidc_provider_name}:aud"
+      values = ["pp-*"]
+    }
+    condition {
+      test = "StringLike"
+      variable = "${local.github_oidc_provider_name}:sub"
+      values = ["repo:perceptionpoint/*"]
+    }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
   }
 }
 
