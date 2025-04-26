d35fdee862533c21b13e38baf33ceb66c4f5fc57
Oron Gola <oron.gola@perception-point.io>
2025-04-26 21:34:10 +0300

    add oidc_provider output

--- output.tf
+++ output.tf
@@ -9,12 +9,19 @@ output "eks_cluster_ca" {
 output "eks_node_sg_id" {
   value = module.security.eks_node_sg
 }
+
 output "karpenter_node_instance_profile_name" {
   value = (var.enable_karpenter_creation) ? module.karpenter[0].instance_profile_name : null
 }
+
 output "karpenter_controller_role_arn" {
   value = (var.enable_karpenter_creation) ? module.karpenter[0].iam_role_arn : null
 }
+
 output "karpenter_interruption_queue_name" {
   value = (var.enable_karpenter_creation) ? module.karpenter[0].queue_name : null
+}
+
+output "oidc_provider" {
+  value = aws_iam_openid_connect_provider.oidc-provider
 }
